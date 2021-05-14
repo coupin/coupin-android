239db7c71fa8fbcda03af57046ae88dcf4ecccd5
simileoluwaaluko <simialuko24@gmail.com>
2021-05-14 12:10:09 +0100

    creating review selection UI created some other new UIs

---
+++ app/src/main/java/com/kibou/abisoyeoke_lawal/coupinapp/ReviewSelectionActivity.kt
@@ -0,0 +1,11 @@
+package com.kibou.abisoyeoke_lawal.coupinapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewSelectionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_selection)
+    }
+}
